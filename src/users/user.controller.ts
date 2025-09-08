92510a249436f7b0b2da91e453977396d021c53c
glrmrissi <guilhermerissi04112006@gmail.com>
2025-09-08 19:00:33 -0300

    feat: return results of db

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Delete, Get } from "@nestjs/common";
+import { UserService } from "./user.service";
 
 @Controller('user')
 export class UserController{
+    constructor( private readonly userService: UserService){}
     @Get()
-    getUser(): string {
-        return "success"
+    getUsers() {
+        return this.userService.findAll()
+    }
+
+    @Get(':id')
+    getUser(id: number) {
+        return this.userService.findOne(id)
+    }
+
+    @Delete(':id')
+    deleteUser(id: number) {
+        return this.userService.remove(id)
     }
 }
