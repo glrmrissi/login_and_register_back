758d48211616481c729e05a3b0c25708a433587d
glrmrissi <guilhermerissi04112006@gmail.com>
2025-09-15 20:44:08 -0300

    feat: add log interceptor for request logging and execution time tracking

---
+++ src/interceptors/log.interceptor.ts
@@ -0,0 +1,17 @@
+import { CallHandler, ExecutionContext, NestInterceptor } from "@nestjs/common";
+import { Observable, tap } from "rxjs";
+
+export class LogInterceptor implements NestInterceptor {
+    intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+
+        const request = context.switchToHttp().getRequest();
+
+        const dt = Date.now();
+
+        return next.handle().pipe(tap(() => {
+            console.log(
+                `\x1b[36mMETHOD:\x1b[0m \x1b[35m[${request.method}]\x1b[0m - \x1b[32mURL:\x1b[0m ${request.url} - Execução levou: \x1b[33m${Date.now() - dt}ms\x1b[0m.`
+            );
+        }))
+    }
+}
