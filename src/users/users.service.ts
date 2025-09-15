84445d1cc3d490a9b6111e354a2edf9033657995
glrmrissi <guilhermerissi04112006@gmail.com>
2025-09-15 20:44:26 -0300

    feat: implement password hashing in user creation and validation methods

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,6 +5,7 @@ import { User } from "src/entities/user.entity";
 import { Repository } from "typeorm";
 import { CreateUserDTO } from "./dto/create-user.dto";
 import { LoginDTO } from './dto/login-dto';
+import { Role } from 'src/enums/role.enum';
 
 @Injectable()
 export class UserService {
@@ -25,14 +26,22 @@ export class UserService {
     }
 
     async create({ name, email, password, role }: CreateUserDTO): Promise<User> {
+        const salt = await bcrypt.genSalt()
+        
+        password = await bcrypt.hash(password, salt)
+
         const user = this.userRepository.create({ name, email, password, role });
         return this.userRepository.save(user);
     }
     
-    async validateUser({ email, password }: LoginDTO): Promise<User | null> {
+    async validateUser({ email, password }: LoginDTO) {
         const user = await this.userRepository.findOneBy({ email });
-        // if (user && await bcrypt.compare(password, user.password)) { // TODO: Use bcrypt to hash and compare passwords and create _ 
-        // new database migration to update existing passwords
+        if (user && await bcrypt.compare(password, user.password)) { // TODO: Use bcrypt to hash and compare passwords and create a new database migration to update existing passwords
+            // return user;
+            // For security reasons, do not return the password
+            const { password, ...result } = user;
+            return result;
+        }
         if (user && password) {
             return user;
         }
