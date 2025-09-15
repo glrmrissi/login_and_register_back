7853a515ade04de9d7cadb1dee46d3cc7274b0bb
glrmrissi <guilhermerissi04112006@gmail.com>
2025-09-15 14:12:01 -0300

    feat: enable CORS for localhost in main application

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,11 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+      origin: ['http://localhost:4200'],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
+      credentials: true,
+  })
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
