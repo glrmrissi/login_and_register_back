7ae5ff2af4e34b7930e4bcbbc398050eb45cedaa
glrmrissi <guilhermerissi04112006@gmail.com>
2025-09-27 20:23:31 -0300

    feat: create KanbanBoard entity with necessary fields and validations

---
+++ src/entities/kanban-boards.entity.ts
@@ -0,0 +1,24 @@
+import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('boards')
+export class KanbanBoard {
+    @IsNotEmpty()
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    name: string;
+
+    @IsString()
+    description: string;
+
+    @CreateDateColumn({ name: 'created_at' })
+    created_at: Date;
+
+    @UpdateDateColumn({ name: 'updated_at' })
+    updated_at: Date;
+
+    @Column({ type: 'int', default: 0 })
+    created_by: number;
+}
