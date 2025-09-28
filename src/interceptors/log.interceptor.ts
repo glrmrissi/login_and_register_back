ee6e8a906337bfde57dd6202942b703005185b78
glrmrissi <guilhermerissi04112006@gmail.com>
2025-09-28 18:24:16 -0300

    feat: enhance logging in LogInterceptor with color-coded HTTP methods

--- src/interceptors/log.interceptor.ts
+++ src/interceptors/log.interceptor.ts
@@ -1,11 +1,16 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from "@nestjs/common";
 import { Observable, tap } from "rxjs";
 
-
-// Interceptor para logar todas as requisições
-
 export class LogInterceptor implements NestInterceptor {
     intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+
+        const methodColors: Record<string, string> = {
+            GET: "\x1b[35m",    // roxo
+            POST: "\x1b[32m",   // verde
+            PUT: "\x1b[33m",    // amarelo
+            PATCH: "\x1b[38;5;208m", // laranja
+            DEFAULT: "\x1b[37m" // branco
+        };
 
         const request = context.switchToHttp().getRequest();
 
@@ -13,7 +18,7 @@ export class LogInterceptor implements NestInterceptor {
 
         return next.handle().pipe(tap(() => {
             console.log(
-                `\x1b[36m[Application]\x1b[0m - \x1b[35m[${request.method}]\x1b[0m - \x1b[32mURL:\x1b[0m ${request.url}    Execução levou: \x1b[33m${Date.now() - dt}ms\x1b[0m.`
+                `\x1b[36m[Application]\x1b[0m - ${methodColors[request.method] || methodColors.DEFAULT}[${request.method}]\x1b[0m - \x1b[32mURL:\x1b[0m ${request.url}    Execução levou: \x1b[33m${Date.now() - dt}ms\x1b[0m.`
             );
         }))
     }
